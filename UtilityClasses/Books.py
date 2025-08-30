2d0d1c4b09d7c02f65e5dcf9c342b7cbcc4e7a06
Abhijan-Patra <abhijanpatra32156@gmail.com>
2025-08-30 23:53:26 +0530

    Added issuebook function to Books.pdf

--- UtilityClasses/Books.py
+++ UtilityClasses/Books.py
@@ -22,15 +22,19 @@ def addBook(id:int,title:str,author:str):
     
     myBook.close()
 
-def removeBook(idd:int):
+def removeBook(idd:int)->str:
     myBook=open("/Users/abhijanpatra/Documents/Python/Library Management System/LibraryManagementSystem/Books.txt",'r')
     allData=myBook.readlines()
     myBook.close()
+    book=""
     myBook=open("/Users/abhijanpatra/Documents/Python/Library Management System/LibraryManagementSystem/Books.txt",'w')
     for i in allData:
         if int(i.strip('\n').split("|")[0])!=idd:
             myBook.write(i)
+        else:
+            book=i
     myBook.close()
+    return book
 
 
 def displayBook():
@@ -38,4 +42,36 @@ def displayBook():
     allBooks=myBook.readlines()
     for book in allBooks:
         print(book,end="")
-displayBook()
+def issueBook(id:int):
+    studentName=input("Enter the Name of the Student: ")
+    studentRegNo=input("Enter the RegNo of the Student: ")
+    myStudent=open("/Users/abhijanpatra/Documents/Python/Library Management System/LibraryManagementSystem/Student.txt",'a+')
+    myStudent.seek(0)
+    allStudents=myStudent.readlines()
+    bookIssued=True
+    book=""
+    print(allStudents)
+    if len(allStudents)>0:
+        for student in allStudents:
+            if student=='\n':
+                continue
+            if studentRegNo == student.split("|")[1]:
+                if int(student.strip("\n").split('|')[2])!=id:
+                   pass
+                else:
+                   bookIssued=True
+                   break
+            else:
+                pass
+        else:
+            bookIssued=False
+        if bookIssued==False:
+            book=removeBook(id)
+            myStudent.write(f"{studentName}|{studentRegNo}|{book}")
+            print(f"{book} 1 has been issued succesfully by {studentName}")
+    else:
+        book=removeBook(id)
+        myStudent.write(f"{studentName}|{studentRegNo}|{book}")
+        print(f"{book.strip("\n")}  2 has been issued succesfully by {studentName}")
+    
+    
